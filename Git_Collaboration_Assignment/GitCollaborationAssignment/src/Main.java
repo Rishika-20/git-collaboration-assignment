59ed2a6d24964d8c50c0fdf15d713b32c34ca2ce
Rishika0101 <124979948+Rishika0101@users.noreply.github.com>
2023-04-02 12:53:10 +0530

    Update Main.java

--- Git_Collaboration_Assignment/GitCollaborationAssignment/src/Main.java
+++ Git_Collaboration_Assignment/GitCollaborationAssignment/src/Main.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args)
     {
         System.out.println("Hello, world!");
-        //creating object of person class.
+        //creating object of Person class.
         Person person = new Person();
-        System.out.println("Is the person adult ? " +person.isAdult(21));
+        person.setName("Rishika");
+        person.setAge(21);
+        System.out.println("Is "+ person.getName()+" adult ? " +person.isAdult(person.getAge()));
     }
+
 }
