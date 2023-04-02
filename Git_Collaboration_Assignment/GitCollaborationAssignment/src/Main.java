d8673d9a52f360b0545f53edd6c550f0cdcc5890
Rishika0101 <124979948+Rishika0101@users.noreply.github.com>
2023-04-02 12:09:45 +0530

    Update Main.java

--- Git_Collaboration_Assignment/GitCollaborationAssignment/src/Main.java
+++ Git_Collaboration_Assignment/GitCollaborationAssignment/src/Main.java
@@ -1,15 +1,11 @@
 import java.util.Scanner;
+
 public class Main {
+    
     public static void main(String[] args)
     {
-        System.out.println("Hello, world!");
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the name of person:");
-        String name = scan.nextLine();
-        System.out.println("Enter the age of the person to check whether he/she is adult or not? ");
-        Integer age = scan.nextInt();
-        //creating object of person class.
-        Person person = new Person(name,age);
+        System.out.println("Hello, world!");        
+        Person person = new Person("Rishika",21);
         System.out.println("Is the person adult ? " +person.isAdult(age));
     }
 }
