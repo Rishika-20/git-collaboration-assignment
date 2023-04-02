5adab4e329ddd95c1a9ff0d36d5434a6abeb926b
Rishika0101 <124979948+Rishika0101@users.noreply.github.com>
2023-04-02 12:05:30 +0530

    Update Person.java

--- Git_Collaboration_Assignment/GitCollaborationAssignment/src/Person.java
+++ Git_Collaboration_Assignment/GitCollaborationAssignment/src/Person.java
@@ -3,10 +3,10 @@ public class Person {
      String name;
     Integer age;
     
+    // setting getter and setter.
     public String getName() {
         return name;
-    }
-    
+    }    
     public void setName(String name) {
         this.name = name;
     }
@@ -16,6 +16,7 @@ public class Person {
     public void setAge(Integer age) {
         this.age = age;
     }
+    //creating constructor.
     public Person(String name, Integer age) {
         this.name = name;
         this.age = age;
